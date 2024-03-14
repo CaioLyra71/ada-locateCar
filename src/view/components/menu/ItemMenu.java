7924d2711d1491a8e6aeb419b354e79e7a408610
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-14 15:55:58 -0300

    feat: implementar itens dos menus

---
+++ src/view/components/menu/ItemMenu.java
@@ -0,0 +1,27 @@
+package view.components.menu;
+
+public class ItemMenu {
+    private Integer codigo;
+    private String texto;
+
+    public ItemMenu(Integer codigo, String texto) {
+        this.codigo = codigo;
+        this.texto = texto;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
