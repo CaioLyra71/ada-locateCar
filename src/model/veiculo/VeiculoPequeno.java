9578fc6c862c2813f610d261673312b6ee5785b0
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-15 17:54:38 -0300

    feat: implementar valores padrão no modelo veículo pequeno

--- src/model/veiculo/VeiculoPequeno.java
+++ src/model/veiculo/VeiculoPequeno.java
@@ -3,8 +3,8 @@ package model.veiculo;
 import java.math.BigDecimal;
 
 public class VeiculoPequeno extends Veiculo{
-    public VeiculoPequeno(String nomeVeiculo, String placaVeiculo, Boolean estaDisponivel, BigDecimal valorDiaria) {
-        super(nomeVeiculo, "PEQUENO", placaVeiculo, estaDisponivel, valorDiaria);
+    public VeiculoPequeno(String nomeVeiculo, String placaVeiculo) {
+        super(nomeVeiculo, "PEQUENO", placaVeiculo, true, BigDecimal.valueOf(100.00));
     }
 
     public VeiculoPequeno() {
