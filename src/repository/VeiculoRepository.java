460a3bb12e39c0e353a13a77c617979a7ef8fd17
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-15 17:56:37 -0300

    feat: implementar método para salvar veículo

--- src/repository/VeiculoRepository.java
+++ src/repository/VeiculoRepository.java
@@ -1,16 +1,31 @@
 package repository;
 
+import infra.BandoDadosVeiculo;
+import infra.exceptions.BancoDadosException;
 import model.veiculo.Veiculo;
+import repository.exceptions.RepositorioException;
+
+import java.util.List;
 
 public class VeiculoRepository implements BuscaPorNome<Veiculo> {
-    @Override
-    public Boolean salvar(Veiculo veiculo) {
-        return null;
+    private final BandoDadosVeiculo bandoDadosVeiculo;
+
+    public VeiculoRepository(BandoDadosVeiculo bandoDadosVeiculo) {
+        this.bandoDadosVeiculo = bandoDadosVeiculo;
     }
 
     @Override
-    public Veiculo buscarPorNome(String nome) {
-        return null;
+    public Boolean salvar(Veiculo veiculo) {
+        return bandoDadosVeiculo.salvar(veiculo);
+    }
+
+    @Override
+    public List<Veiculo> buscarPorNome(String nomeVeiculo) throws RepositorioException {
+        try {
+            return bandoDadosVeiculo.buscarPorNome(nomeVeiculo);
+        } catch (BancoDadosException e) {
+            throw new RepositorioException("O veículo não foi encotrado",e);
+        }
     }
 
     @Override
