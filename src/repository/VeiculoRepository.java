64b12001d4171de1a0650f5ee7c5bcc0264cb2f4
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-14 21:17:23 -0300

    feat: implementar interface de busca por nome

--- src/repository/VeiculoRepository.java
+++ src/repository/VeiculoRepository.java
@@ -2,19 +2,29 @@ package repository;
 
 import model.veiculo.Veiculo;
 
-public class VeiculoRepository implements Repository<Veiculo> {
+public class VeiculoRepository implements BuscaPorNome<Veiculo> {
     @Override
-    public Boolean save(Veiculo veiculo) {
+    public Boolean salvar(Veiculo veiculo) {
         return null;
     }
 
     @Override
-    public Veiculo findById(String id) {
+    public Veiculo buscarPorNome(String nome) {
         return null;
     }
 
     @Override
-    public Veiculo update(String id, Veiculo veiculo) {
+    public Veiculo buscarPorId(String id) {
+        return null;
+    }
+
+    @Override
+    public Veiculo atualizar(String id, Veiculo veiculo) {
+        return null;
+    }
+
+    @Override
+    public Boolean deletar(String id) {
         return null;
     }
 }
