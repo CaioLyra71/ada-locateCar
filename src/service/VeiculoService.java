d5a7e5cfd09553b6d29d5c488cb200e5ac622b60
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-15 17:56:52 -0300

    feat: implementar método para salvar veículo

--- src/service/VeiculoService.java
+++ src/service/VeiculoService.java
@@ -1,28 +1,44 @@
 package service;
 
 import model.veiculo.Veiculo;
-import repository.Repository;
+import repository.BuscaPorNome;
+import repository.exceptions.RepositorioException;
+import service.exceptions.ModeloException;
+import service.exceptions.ServicoException;
 
 import java.util.List;
 
 public class VeiculoService {
 
-    private final Repository<Veiculo> veiculoRepository;
+    private final BuscaPorNome<Veiculo> veiculoRepository;
 
-    public VeiculoService(Repository<Veiculo> veiculoRepository) {
+    public VeiculoService(BuscaPorNome<Veiculo> veiculoRepository) {
         this.veiculoRepository = veiculoRepository;
     }
 
     public Boolean salvarVeiculo(Veiculo veiculo){
-        return null;
+        if (veiculo.getNomeVeiculo() == null || veiculo.getNomeVeiculo().trim().isEmpty()) {
+            throw new ModeloException("Nome inválido");
+        }
+        if(veiculo.getPlacaVeiculo() == null || veiculo.getPlacaVeiculo().trim().isEmpty()){
+            throw new ModeloException("Placa inválida");
+        }
+
+        return veiculoRepository.salvar(veiculo);
     }
 
-    public List<Veiculo> buscarVeiculoPorNome(String nomeVeiculo){
-        return null;
-    }
-
-    public Veiculo buscarVeiculoPorId(String idVeiculo){
-        return null;
+    public List<Veiculo> buscarVeiculoPorNome(String nomeVeiculo) throws Exception {
+        List<Veiculo> veiculosEncontrados;
+        if(nomeVeiculo == null || nomeVeiculo.trim().isEmpty()) {
+            throw new Exception("O nome do veículo não pode ser vazio");
+        }
+        // mais validações
+        try {
+            veiculosEncontrados = veiculoRepository.buscarPorNome(nomeVeiculo);
+        } catch (RepositorioException e) {
+            throw new ServicoException("O veículo não foi encontrado.", e);
+        }
+        return veiculosEncontrados;
     }
 
     public Veiculo atualizarVeiculo(Veiculo veiculo, String idVeiculo){
