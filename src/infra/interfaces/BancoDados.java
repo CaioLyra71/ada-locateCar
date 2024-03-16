c58315713948a885434b38d589b657229f0f6d63
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-16 15:00:24 -0300

    feat: retornar valores mais semânticos nos métodos salvar e atualizar

--- src/infra/interfaces/BancoDados.java
+++ src/infra/interfaces/BancoDados.java
@@ -1,10 +1,12 @@
 package infra.interfaces;
 
+import infra.exceptions.BancoDadosException;
+
 import java.util.List;
 
 public interface BancoDados<K, V> {
-    Boolean salvar(V v);
-    Boolean atualizar(K k, V v);
+    V salvar(V v) throws BancoDadosException;
+    V atualizar(K k, V v);
     List<V> listarTodos();
     V buscarPorId(K k);
 }
