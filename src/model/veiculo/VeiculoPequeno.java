aa30c9e753c970626c7898103d2fa1470b2fee6d
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-16 12:53:18 -0300

    feat: melhorar precisão do valor da diária do veículo pequeno

--- src/model/veiculo/VeiculoPequeno.java
+++ src/model/veiculo/VeiculoPequeno.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public class VeiculoPequeno extends Veiculo{
     public VeiculoPequeno(String nomeVeiculo, String placaVeiculo) {
-        super(nomeVeiculo, "PEQUENO", placaVeiculo, true, BigDecimal.valueOf(100.00));
+        super(nomeVeiculo, "PEQUENO", placaVeiculo, true, new BigDecimal("100.00"));
     }
 
     public VeiculoPequeno() {
