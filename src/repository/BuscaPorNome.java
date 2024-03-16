7a03d3a5bcb8e2e2597b1bc99c2d1ce324d50c97
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-16 15:02:58 -0300

    feat: separar interfaces para melhor acomodar implementação dos métodos

--- src/repository/BuscaPorNome.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import repository.exceptions.RepositorioException;
-
-import java.util.List;
-
-public interface BuscaPorNome<T> extends Repository<T> {
-    List<T> buscarPorNome(String nome) throws RepositorioException;
-}
--- src/repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-public interface Repository<T>{
-    Boolean salvar(T t);
-    T buscarPorId(String id);
-    T atualizar(String id, T t);
-    Boolean deletar(String id);
-}
