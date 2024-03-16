94df6282e7780259b1162799e02ad3aad646c738
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-16 12:57:28 -0300

    feat: adicionar exceção de veículo não encontrado nos models

---
+++ src/model/exceptions/VeiculoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package model.exceptions;
+
+public class VeiculoNaoEncontradoException extends RuntimeException{
+    public VeiculoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
