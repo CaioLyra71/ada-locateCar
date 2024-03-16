ff1263fbc2895942ba3ba8c7c6404346a187d39d
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-16 14:23:40 -0300

    feat: adicionar método de listar todos e buscar por Id

--- src/infra/interfaces/BancoDados.java
+++ src/infra/interfaces/BancoDados.java
@@ -1,6 +1,10 @@
 package infra.interfaces;
 
+import java.util.List;
+
 public interface BancoDados<K, V> {
     Boolean salvar(V v);
     Boolean atualizar(K k, V v);
+    List<V> listarTodos();
+    V buscarPorId(K k);
 }
