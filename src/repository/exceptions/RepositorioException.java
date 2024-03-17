e4e71c30bcee80f7d36e2085f36e17c5c3c736bb
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-17 01:38:57 -0300

    feat: adicionar novo construtor que recebe apenas mensagem

--- src/repository/exceptions/RepositorioException.java
+++ src/repository/exceptions/RepositorioException.java
@@ -4,4 +4,7 @@ public class RepositorioException extends Exception{
     public RepositorioException(String message, Exception cause) {
         super(message, cause);
     }
+    public RepositorioException(String message) {
+        super(message);
+    }
 }
