779ff5edd14d1a1e573f25bd7569d1d75cfe0768
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-17 01:33:26 -0300

    feat: implementar funcionalidade de listagem dos clientes pessoa física cadastrados

---
+++ src/view/cliente/util/MostrarTodosPessoaFisica.java
@@ -0,0 +1,29 @@
+package view.cliente.util;
+
+import model.cliente.ClientePessoaFisica;
+import view.components.tabela.TabelaView;
+
+import java.util.List;
+
+public class MostrarTodosPessoaFisica {
+    private String[] cabecalhos;
+    private String[][] linhas;
+    private List<ClientePessoaFisica> clientes;
+
+    public MostrarTodosPessoaFisica(String[] cabecalhos, List<ClientePessoaFisica> clientes) {
+        this.cabecalhos = cabecalhos;
+        this.clientes = clientes;
+        this.linhas = new String[clientes.size()][cabecalhos.length];
+    }
+
+    public void executar() {
+        for (int i = 0; i < clientes.size(); i++) {
+            ClientePessoaFisica cliente = clientes.get(i);
+            linhas[i][0] = cliente.getNomeCliente();
+            linhas[i][1] = cliente.getCpf();
+        }
+
+        TabelaView tabelaView = new TabelaView(cabecalhos, linhas);
+        tabelaView.executar();
+    }
+}
