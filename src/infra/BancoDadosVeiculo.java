e736843064dc5d12a6ab2ae57bb910345a2e4976
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-17 14:04:57 -0300

    feat: implementar funcionalidades de banco de dados para veículos

--- src/infra/BancoDadosVeiculo.java
+++ src/infra/BancoDadosVeiculo.java
@@ -4,40 +4,55 @@ import infra.exceptions.BancoDadosException;
 import infra.interfaces.BancoDadosBuscaNome;
 import model.veiculo.Veiculo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BancoDadosVeiculo implements BancoDadosBuscaNome<String, Veiculo> {
-    private final Map<String, Veiculo> veiculos = new HashMap<>();
+    private final static Map<String, Veiculo> veiculos = new HashMap<>();
 
     @Override
     public Veiculo salvar(Veiculo veiculo) throws BancoDadosException {
-        Veiculo veiculoASalvar = veiculos.put(veiculo.getPlacaVeiculo(), veiculo);
-        if(veiculoASalvar == null) {
-            throw new BancoDadosException("Falha ao tentar salvar o veículo");
+        Veiculo veiculoASalvar = buscarPorId(veiculo.getPlacaVeiculo());
+        if (veiculoASalvar != null) {
+            throw new BancoDadosException("Veículo já existe.");
         }
-        return veiculoASalvar;
+        return veiculos.put(veiculo.getPlacaVeiculo(), veiculo);
     }
 
     @Override
-    public Veiculo atualizar(String s, Veiculo veiculo) {
-        return null;
+    public Veiculo atualizar(String idVeiculo, Veiculo veiculo) {
+        if(!idVeiculo.equals(veiculo.getPlacaVeiculo())){
+            veiculos.remove(idVeiculo);
+        }
+        return veiculos.put(veiculo.getPlacaVeiculo(), veiculo);
     }
 
     @Override
     public List<Veiculo> listarTodos() {
-        return null;
+        List<Veiculo> registros = new ArrayList<>();
+        var entries = veiculos.entrySet();
+        for (var entry : entries) {
+            registros.add(entry.getValue());
+        }
+        return registros;
     }
 
     @Override
-    public Veiculo buscarPorId(String s) {
-        return null;
+    public Veiculo buscarPorId(String idVeiculo) {
+        return veiculos.get(idVeiculo);
     }
 
     @Override
-    public List<Veiculo> buscarPorNome(String nomeVeiculo) throws BancoDadosException {
-        //lógica para buscar por nome
-        throw new BancoDadosException("Veículo não foi encontrado");
+    public List<Veiculo> buscarPorNome(String nomeVeiculo) {
+        List<Veiculo> todosVeiculos = listarTodos();
+        List<Veiculo> veiculosEncontrados = new ArrayList<>();
+        for (Veiculo veiculo : todosVeiculos) {
+            if (veiculo.getNomeVeiculo().toLowerCase().contains(nomeVeiculo.toLowerCase())) {
+                veiculosEncontrados.add(veiculo);
+            }
+        }
+        return veiculosEncontrados;
     }
 }
