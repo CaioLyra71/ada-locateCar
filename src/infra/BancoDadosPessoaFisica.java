bccf2158d00b5855d5d19eaca9a0cdc1897eab20
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-17 01:28:48 -0300

    feat: implementar banco de dados para clientes pessoa física

---
+++ src/infra/BancoDadosPessoaFisica.java
@@ -0,0 +1,48 @@
+package infra;
+
+import infra.exceptions.BancoDadosException;
+import infra.interfaces.BancoDados;
+import model.cliente.ClientePessoaFisica;
+import repository.exceptions.RepositorioException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BancoDadosPessoaFisica implements BancoDados<String, ClientePessoaFisica> {
+
+    private static final Map<String, ClientePessoaFisica> clientes = new HashMap<>();
+
+    @Override
+    public ClientePessoaFisica salvar(ClientePessoaFisica cliente) throws BancoDadosException {
+        ClientePessoaFisica clienteExistente = buscarPorId(cliente.getCpf());
+        if (clienteExistente != null) {
+            throw new BancoDadosException("Cliente já existe.");
+        }
+        return clientes.put(cliente.getCpf(), cliente);
+    }
+
+    @Override
+    public ClientePessoaFisica atualizar(String idCliente, ClientePessoaFisica clientePessoaFisica) {
+        if (!idCliente.equals(clientePessoaFisica.getCpf())) {
+            clientes.remove(idCliente);
+        }
+        return clientes.put(clientePessoaFisica.getCpf(), clientePessoaFisica);
+    }
+
+    @Override
+    public List<ClientePessoaFisica> listarTodos() {
+        List<ClientePessoaFisica> clientesPessoaFisica = new ArrayList<>();
+        var entries = clientes.entrySet();
+        for (var entry : entries) {
+            clientesPessoaFisica.add(entry.getValue());
+        }
+        return clientesPessoaFisica;
+    }
+
+    @Override
+    public ClientePessoaFisica buscarPorId(String idCliente) {
+        return clientes.get(idCliente);
+    }
+}
